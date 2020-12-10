17892adb0fdcea799414f40b47da5af3750b539c
fantimond <fantimond@gmail.com>
2020-12-11 00:35:04 +0800

    reformat

--- ios_app/StyleTransfer/StyleTransferInput.swift
+++ ios_app/StyleTransfer/StyleTransferInput.swift
@@ -7,23 +7,23 @@
 import CoreML
 
 class StyleTransferInput : MLFeatureProvider {
-    
-    var input: CVPixelBuffer
-    
-    var featureNames: Set<String> {
-        get {
-            return ["img_placeholder__0"]
-        }
+  
+  var input: CVPixelBuffer
+  
+  var featureNames: Set<String> {
+    get {
+      return ["img_placeholder__0"]
     }
-    
-    func featureValue(for featureName: String) -> MLFeatureValue? {
-        if (featureName == "img_placeholder__0") {
-            return MLFeatureValue(pixelBuffer: input)
-        }
-        return nil
+  }
+  
+  func featureValue(for featureName: String) -> MLFeatureValue? {
+    if (featureName == "img_placeholder__0") {
+      return MLFeatureValue(pixelBuffer: input)
     }
-    
-    init(input: CVPixelBuffer) {
-        self.input = input
-    }
+    return nil
+  }
+  
+  init(input: CVPixelBuffer) {
+    self.input = input
+  }
 }
